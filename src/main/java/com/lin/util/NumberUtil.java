7c0f73608ded9ba75aba3ac3f02b6e868661c0ee
刘效佳 <liuxiaojia@blacklake.cn>
2022-04-09 10:21:00 +0800

    1

---
+++ src/main/java/com/lin/util/NumberUtil.java
@@ -0,0 +1,13 @@
+package com.lin.util;
+
+/**
+ * @author liuxiaojia
+ * @date 2022/4/9 10:20
+ * @Description:
+ */
+public class NumberUtil {
+
+    public static void main(String[] args) {
+        System.out.println(2);
+    }
+}
