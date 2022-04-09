5dccb6ff55884c314817c6ca8b5430623a65b6c7
刘效佳 <liuxiaojia@blacklake.cn>
2022-04-09 10:19:05 +0800

    Initial commit

---
+++ src/main/java/com/lin/util/StrUtil.java
@@ -0,0 +1,13 @@
+package com.lin.util;
+
+/**
+ * @author liuxiaojia
+ * @date 2022/4/9 09:53
+ * @Description:
+ */
+public class StrUtil {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
