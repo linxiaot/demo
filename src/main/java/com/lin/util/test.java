ea2503bef6dd13d03db6f6182585194dcc18337b
liuxiaojia <liuxiaojia@blacklake.cn>
2022-04-29 21:15:30 +0800

    1

--- src/main/java/com/lin/util/test.java
+++ src/main/java/com/lin/util/test.java
@@ -1,5 +1,9 @@
 package com.lin.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author liuxiaojia
  * @date 2022/4/9 10:29
@@ -7,4 +11,13 @@ package com.lin.util;
  */
 public class test {
 
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        List<Integer> integers = list.stream().filter(it -> it.equals(2))
+            .collect(Collectors.toList());
+        list.add(4);
+    }
 }
