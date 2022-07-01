1bcb860563b482a91549b98d157357201428a9b7
liuxiaojia <liuxiaojia@blacklake.cn>
2022-07-01 09:12:42 +0800

    Revert "1"
    This reverts commit ea2503bef6dd13d03db6f6182585194dcc18337b.

--- src/main/java/com/lin/util/test.java
+++ src/main/java/com/lin/util/test.java
@@ -1,9 +1,5 @@
 package com.lin.util;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * @author liuxiaojia
  * @date 2022/4/9 10:29
@@ -11,13 +7,4 @@ import java.util.stream.Collectors;
  */
 public class test {
 
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        List<Integer> integers = list.stream().filter(it -> it.equals(2))
-            .collect(Collectors.toList());
-        list.add(4);
-    }
 }
